0a09173a38ce3667343e9e575a75fc68d772b7e5
Striker <andreneves2005@outlook.com>
2026-09-21 00:24:42 -0300

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.striker.dotoapp',
+  appName: 'ToDo',
+  webDir: 'dist'
+};
+
+export default config;
